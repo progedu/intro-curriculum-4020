ad5336c2520b5b630035c88a22c144f8cd0545ed
G3-Hobbit <g3yarinaoshi@gmail.com>
2017-03-08 18:49:09 +0900

    Promise チェインの練習

--- index.js
+++ index.js
@@ -20,3 +20,8 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+getA().then((a) => { // 11
+  return getB().then((b) => { return a * b; }); // 11 * 13 = 143
+}).then((result) => {
+  return getC().then((c) => { return result * c });  // 143 * 17 = 2431
+}).then((result) => { console.log(result); });  //  結果出力
