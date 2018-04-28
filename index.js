c17a3a91bb02993980f7a54f81280191b296c564
metamorLP <faire_laissez@yahoo.co.jp>
2018-04-28 16:48:47 +0000

    Promise チェインを利用して標準出力するよう実装

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+
+getA().then(function(a) {
+  return getB().then(function(b) { return a * b; });
+}) .then(function(ab) {
+  return getC().then(function(c) { console.log( ab * c ) });
+});
+
+
