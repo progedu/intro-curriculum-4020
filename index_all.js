42f2fa0e4942487c0b3d74c9878c043adde0e5bc
Kazuyuki Matsuda <mtdkzyk.github@gmail.com>
2020-02-10 16:51:46 +0900

    別の書き方

---
+++ index_all.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function getA() {
+  return new Promise((resolve) => {
+    setTimeout(() => { resolve(11); }, 1000);
+  });
+}
+
+function getB() {
+  return new Promise((resolve) => {
+    setTimeout(() => { resolve(13); }, 1000);
+  });
+}
+
+function getC() {
+  return new Promise((resolve) => {
+    setTimeout(() => { resolve(17); }, 1000);
+  });
+}
+
+// TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
+// ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+Promise.all([getA(), getB(), getC()]).then((results) => {
+  console.log(results[0] * results[1] * results[2]);
+});
