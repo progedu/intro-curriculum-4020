0360860f5c06d7bdd209c5488ca63e44896f2a75
nuts3745 <kohji_suzuki@me.com>
2020-04-08 19:59:32 +0900

    Promiseチェインを利用して計算して出力

--- index.js
+++ index.js
@@ -20,3 +20,15 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+
+getA().then((a) => { // a は 11
+	return getB().then((b) => {
+		return a * b;
+	});
+}).then((ab) => { // ab は 11 * 13
+	return getC().then((c) => {
+		return ab * c;
+	});
+}).then((abc) => { // abc は 11 * 13 * 17
+	console.log(abc); // 2431 が出力される
+});
