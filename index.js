b6d92d34b5fcc0ce417cc0fc0da961372e3f5d26
shinfujita <shin4522@gmail.com>
2020-05-05 13:36:40 +0900

    4章20節の課題

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { get } from "http";
+
 function getA() {
   return new Promise((resolve) => {
     setTimeout(() => { resolve(11); }, 1000);
@@ -17,6 +19,22 @@ function getC() {
     setTimeout(() => { resolve(17); }, 1000);
   });
 }
+
+getA().then((a) => {
+  return getB().then((b) => { return a * b; });
+}).then((r) => {
+  getC().then((c) => { console.log(r * c); });
+});
+
+// getA().then(async(a) => {
+//   const b = await getB();
+//   const c = await getC();
+//   console.log(a * b * c);
+// })
+
+// Promise.all([getA(), getB(), getC()]).then((r) => {
+//   console.log(r[0] * r[1] * r[2]);
+// });
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
