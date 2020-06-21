5df21d915a0bf3ebb5759fb238a2e0b7ab6fa8d2
Motonori Banno <millibarjp@gmail.com>
2020-06-21 17:39:20 +0900

    getA * getB * getC をコンソールに出力する

--- index.js
+++ index.js
@@ -20,3 +20,12 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+getA().then((a) => {
+  const promiseAB = getB().then((b) => { return a * b });
+  return promiseAB;
+}).then((ab) => {
+  const promiseABC = getC().then((c) => { return ab * c });
+  return promiseABC;
+}).then((abc) => {
+  console.log(abc);
+});
