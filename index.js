2fd8929f962fc0d7b551279d847bdb29c01d93dc
blue-one1975 <hiroshitakeda428@yahoo.co.jp>
2020-07-17 16:12:18 +0900

    Promiseチェーンの練習

--- index.js
+++ index.js
@@ -20,3 +20,20 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+getA().then((a) => {
+  return getB().then((b) => { return a * b});
+}).then((ab) => {
+ getC().then((c) => {
+   console.log(ab * c);
+ });
+});
+
+Promise.all([getA(), getB(), getC(),]).then(result => {
+ console.log(result[0] * result[1] * result[2]);
+});
+
+getA().then(async a => {
+ const b = await getB();
+ const c = await getC();
+ console.log(a * b * c);
+});
