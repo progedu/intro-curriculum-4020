ee7d281e1b259c42180b1e3546d62474dd877df0
anazsama <anazsama@outlook.jp>
2020-09-26 14:16:03 +0900

    Promiseチェインの練習

--- index.js
+++ index.js
@@ -20,3 +20,40 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+
+/*
+getA().then((a) => {
+  console.log("★a★" + a + "★★★");
+  getB().then((b) => {
+    console.log("★b★" + b + "★★★");
+    console.log("★a*b★" + a * b + "★★★");
+    getC().then((c) => {
+      console.log("★c★" + c + "★★★");
+      console.log("★a*b*c★" + a * b * c + "★★★");
+    })
+  })
+});
+*/
+
+/*
+getA().then((a) => {
+  console.log("★a★" + a + "★★★");
+  return getB().then((b) => {
+    console.log("★b★" + b + "★★★");
+    console.log("★a*b★" + a * b + "★★★");
+    return a * b;
+  });
+}).then((ab) => {
+  getC().then((c) => {
+    console.log("★c★" + c + "★★★");
+    console.log("★a*b*c★" + ab * c + "★★★");
+  });
+});
+*/
+
+Promise.all([getA(), getB(), getC()]).then((results) => {
+  console.log("getA():" + results[0]);
+  console.log("getB():" + results[1]);
+  console.log("getC():" + results[2]);
+  console.log("getA()*getB()*getC():" + (results[0] * results[1] * results[2]));
+})
