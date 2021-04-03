42fea29e0abf835cae101c220277974569509475
shrine <shrine.game1@gmail.com>
2021-04-03 17:00:30 +0900

    サービス開発6の練習

--- index.js
+++ index.js
@@ -20,3 +20,26 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+
+// 3秒
+getA().then(a => {
+  return getB().then(b => {
+    return a * b;
+  });
+}).then(ab => {
+  return getC().then(c => {
+    console.log(ab * c);
+  });
+});
+
+// 1秒
+Promise.all([getA(), getB(), getC()]).then(result => {
+  console.log(result[0] * result[1] * result[2]);
+});
+
+// 3秒
+getA().then(async a => {
+  const b = await getB();
+  const c = await getC();
+  console.log(a * b * c);
+});
